73425b8a187c62f442180e36d4f7db9b161bf789
Salman <salman26080@gmail.com>
2024-12-23 07:54:56 +0800

    different structure again

--- src/Components/Clients/Clients.jsx
+++ src/Components/Clients/Clients.jsx
@@ -1,47 +1,27 @@
 import React from "react";
 import "./Clients.css";
+import photo1 from "../../assets/photo-1.png";
+import photo2 from "../../assets/photo-2.png";
+import photo3 from "../../assets/photo-3.png";
+import photo4 from "../../assets/photo-4.png";
+import photo5 from "../../assets/photo-5.png";
+import photo6 from "../../assets/photo-6.png";
+import photo7 from "../../assets/photo-7.png";
+import photo8 from "../../assets/photo-8.png";
+import photo9 from "../../assets/photo-9.png";
+import photo10 from "../../assets/photo-10.png";
 
 const clientsData = [
-  {
-    image: "/src/assets/photo-1.png",
-    name: "Rekamitra Sukses Investama",
-  },
-  {
-    image: "/src/assets/photo-2.png",
-    name: "Rekamitra Cipta Tambang",
-  },
-  {
-    image: "/src/assets/photo-3.png",
-    name: "Prakasita Citra Buana",
-  },
-  {
-    image: "/src/assets/photo-4.png",
-    name: "Garsindo Engineering Equipment",
-  },
-  {
-    image: "/src/assets/photo-5.png",
-    name: "Amanah Tour",
-  },
-  {
-    image: "/src/assets/photo-6.png",
-    name: "CV. Rahmania Bagaskara Gemilang",
-  },
-  {
-    image: "/src/assets/photo-7.png",
-    name: "Arsolusi Consulting Group",
-  },
-  {
-    image: "/src/assets/photo-8.png",
-    name: "Cinta Puri Pratama",
-  },
-  {
-    image: "/src/assets/photo-9.png",
-    name: "PT. Transworld Solution",
-  },
-  {
-    image: "/src/assets/photo-10.png",
-    name: "Others",
-  },
+  { image: photo1, name: "Rekamitra Sukses Investama" },
+  { image: photo2, name: "Rekamitra Cipta Tambang" },
+  { image: photo3, name: "Prakasita Citra Buana" },
+  { image: photo4, name: "Garsindo Engineering Equipment" },
+  { image: photo5, name: "Amanah Tour" },
+  { image: photo6, name: "CV. Rahmania Bagaskara Gemilang" },
+  { image: photo7, name: "Arsolusi Consulting Group" },
+  { image: photo8, name: "Cinta Puri Pratama" },
+  { image: photo9, name: "PT. Transworld Solution" },
+  { image: photo10, name: "Others" },
 ];
 
 const Clients = () => {
