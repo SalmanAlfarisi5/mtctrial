21fc7b1c65435af454de64788e50b510a3aaa0f6
Salman <salman26080@gmail.com>
2024-12-27 18:36:28 +0800

    Yet another config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import Hero from './Components/Hero/Hero';
 import Programs from './Components/Programs/Programs';
