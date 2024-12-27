f04e29a917872c6025fa10fa4a869ae9383d8380
Salman <salman26080@gmail.com>
2024-12-27 18:17:07 +0800

    preparation for changing to multi page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import Hero from './Components/Hero/Hero';
 import Programs from './Components/Programs/Programs';
-import Title from './Components/Title/Title';
 import About from './Components/About/About';
 import Clients from './Components/Clients/Clients';
 import Teams from './Components/Teams/Teams';
 import Contact from './Components/Contact/Contact';
 import Footer from './Components/Footer/Footer';
-import WhatsappWidget from './Components/WhatsappWidget/WhatsappWidget';
-import ChatbotWidget from './Components/ChatbotWidget/ChatbotWidget';
 
 const App = () => {
   return (
-    <div>
+    <Router>
       <Navbar />
-      <Hero />
-      <div className="container">
-        <Title subTitle="Our programs" title="What we offer" />
-        <Programs />
-        <About />
-        <Title subTitle="Clients" title="Companies that Choose Us" />
-        <Clients />
-        <Title subTitle="" title="Our Teams" />
-        <Teams />
-        <Title subTitle="Contact Us" title="Get in Touch" />
-        <Contact />
-        <Footer />
-      </div>
-      <ChatbotWidget />
-      <WhatsappWidget /> 
-    </div>
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/programs" element={<Programs />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/clients" element={<Clients />} />
+        <Route path="/teams" element={<Teams />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 };
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,38 +1,29 @@
-import React, { useEffect, useState } from 'react'
-import './Navbar.css'
-import logo from '../../assets/logo.png'
-import { Link } from 'react-scroll';
-import menu_icon from '../../assets/menu-icon.png'
+import React, { useState } from 'react';
+import './Navbar.css';
+import logo from '../../assets/logo.png';
+import menu_icon from '../../assets/menu-icon.png';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-
-  const [sticky, setSticky] = useState(false);
-
-  useEffect(() => {
-    window.addEventListener('scroll', () =>{
-      window.scrollY > 50 ? setSticky(true) : setSticky(false);
-    })
-  },[])
-
   const [mobileMenu, setMobileMenu] = useState(false);
   const toggleMenu = () => {
-    mobileMenu? setMobileMenu(false) : setMobileMenu(true);
-  }
+    setMobileMenu(!mobileMenu);
+  };
 
   return (
-    <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
-       <img src={logo} alt="" className='logo'/>
-       <ul className={mobileMenu? '':'hide-mobile-menu'}>
-        <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
-        <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
-        <li><Link to='about' smooth={true} offset={-150} duration={500}>About</Link></li>
-        <li><Link to='clients' smooth={true} offset={-260} duration={500}>Clients</Link></li>
-        <li><Link to='teams' smooth={true} offset={-260} duration={500}>Teams</Link></li>
-        <li><Link to='contact' smooth={true} offset={-260} duration={500}className='btn'>Contact us</Link></li>
-       </ul>
-       <img src={menu_icon} alt="" className='menu-icon' onClick={toggleMenu}/>
+    <nav className="container">
+      <img src={logo} alt="Logo" className="logo" />
+      <ul className={mobileMenu ? '' : 'hide-mobile-menu'}>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/programs">Programs</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/clients">Clients</Link></li>
+        <li><Link to="/teams">Teams</Link></li>
+        <li><Link to="/contact" className="btn">Contact Us</Link></li>
+      </ul>
+      <img src={menu_icon} alt="Menu" className="menu-icon" onClick={toggleMenu} />
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
