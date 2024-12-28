74e7c03acd8b854c90a3da4f22b4a39e1a222b49
Salman <salman26080@gmail.com>
2024-12-28 12:00:16 +0800

    hero page

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
-import './Hero.css'
+import React from 'react';
+import './Hero.css';
+import { useLanguage } from '../../LanguageContext';
 
 const Hero = () => {
-  return (
-    <div className='hero container'>
-        <div className="hero-text">
-            <h1>Tax Consultation for your Businesses</h1>
-            <a href="https://drive.google.com/file/d/1VKNeaj9ISnw_zEUqvzgxsu3fbFvhX7O2/view?usp=sharing" 
-            target="_blank" rel="noopener noreferrer" className="btn"> Explore more 
-            </a>
-        </div>
-    </div>
-  )
-}
+  const { language } = useLanguage(); // Get the current language
 
-export default Hero
+  return (
+    <div className="hero container">
+      <div className="hero-text">
+        <h1>
+          {language === "en"
+            ? "Tax Consultation for your Businesses"
+            : "Konsultasi Pajak untuk Bisnis Anda"}
+        </h1>
+        <a
+          href="https://drive.google.com/file/d/1VKNeaj9ISnw_zEUqvzgxsu3fbFvhX7O2/view?usp=sharing"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="btn"
+        >
+          {language === "en" ? "Explore more" : "Baca selengkapnya"}
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
